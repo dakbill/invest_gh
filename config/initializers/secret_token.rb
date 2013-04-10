b823148d681839fa62c05b31b625f12306012df6
dakbill <dakbill@yahoo.com>
2013-04-10 22:30:42 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InvestGh::Application.config.secret_token = '4a29c2a9843a88c84c32ed952bec39e338b93d4e93c5dd3a7f59b5db9c755c304af9b4204465d37e97c6a5cac3ad29115a46d1f242e23072d44f5e783f94121e'
